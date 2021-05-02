038f70a90d6d6fba9e8851875c678228a2ee3d7a
dherv <damien.herv@gmail.com>
2021-05-02 17:02:27 +0900

    chore: add pwa config

--- webpack.common.js
+++ webpack.common.js
@@ -1,32 +1,43 @@
 const path = require("path");
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WorkboxPlugin = require("workbox-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+
 module.exports = {
   entry: "./src/index.js",
-  mode: "development",
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /(node_modules|bower_components)/,
         loader: "babel-loader",
-        options: { presets: ["@babel/env"] }
+        options: { presets: ["@babel/env"] },
       },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"]
-      }
-    ]
+        use: ["style-loader", "css-loader"],
+      },
+    ],
   },
   resolve: { extensions: ["*", ".js", ".jsx"] },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Weather App',
+      title: "Weather App",
       template: "./public/index.html",
-      filename: "./index.html"
+      filename: "./index.html",
+      inject: "head",
+    }),
+    new WorkboxPlugin.GenerateSW(),
+    new CopyPlugin({
+      patterns: [
+        { from: "public/webmanifest.json", to: "." },
+        { from: "public/icons/", to: "./icons" }
+      ],
     }),
   ],
   output: {
     path: path.resolve(__dirname, "dist/"),
-    filename: "bundle.js"
-  }
+    filename: "[name].bundle.js",
+    clean: true,
+  },
 };
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,10 +1,14 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 const TerserPlugin = require('terser-webpack-plugin');
+const { WebpackManifestPlugin } = require('webpack-manifest-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
+  plugins: [
+    new WebpackManifestPlugin()
+  ],
   optimization: {
-    minimizer: [new TerserPlugin({ /* additional options here */ })],
+    minimizer: [new TerserPlugin()],
   },
 });
